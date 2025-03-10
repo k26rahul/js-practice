705eb162a0d95372b555e0b47ee48e9d97e3aff9
Rahul Maurya <78677484+k26rahul@users.noreply.github.com>
2025-03-10 23:37:26 +0530

    vite demo done

---
+++ vite-demo/src/views/About.vue
@@ -0,0 +1,4 @@
+<template>
+  <h1>this is about page</h1>
+  <p>i am Rahul, and Vidu is my bro</p>
+</template>
---
+++ vite-demo/src/views/Home.vue
@@ -0,0 +1,4 @@
+<template>
+  <h1>this is home page</h1>
+  <p>welcome to my website</p>
+</template>
